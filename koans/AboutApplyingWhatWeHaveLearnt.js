ff8dd987b940f98e42e92dcfa966363dcdd88aeb
sonim1 <bumfoo@gmail.com>
2017-08-25 00:38:16 +0900

    modified my homework

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -14,100 +14,128 @@ describe("About Applying What We Have Learnt", function() {
     ];
   });
 
-  /*********************************************************************************/
+/*********************************************************************************/
 
-  it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (imperative)", function () {
+it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (imperative)", function () {
 
-    var i,j,hasMushrooms, productsICanEat = [];
+    var i, j, hasMushrooms, productsICanEat = [];
 
-    for (i = 0; i < products.length; i+=1) {
+    for (i = 0; i < products.length; i += 1) {
         if (products[i].containsNuts === false) {
             hasMushrooms = false;
-            for (j = 0; j < products[i].ingredients.length; j+=1) {
-               if (products[i].ingredients[j] === "mushrooms") {
-                  hasMushrooms = true;
-               }
+            for (j = 0; j < products[i].ingredients.length; j += 1) {
+                if (products[i].ingredients[j] === "mushrooms") {
+                    hasMushrooms = true;
+                }
             }
             if (!hasMushrooms) productsICanEat.push(products[i]);
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
-  });
+    expect(productsICanEat.length).toBe(1);
+});
 
-  it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
+it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
 
-      var productsICanEat = [];
+    var productsICanEat = [];
 
-      /* solve using filter() & all() / any() */
+    /* solve using filter() & all() / any() */
+    productsICanEat = _(products).filter(function (x) {
+            return x["containsNuts"] === false && _(x['ingredients']).all(function (x) { return x !== 'mushrooms'; });
+        })
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
-  });
+    expect(productsICanEat.length).toBe(1);
+});
 
-  /*********************************************************************************/
+/*********************************************************************************/
 
-  it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (imperative)", function () {
+it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (imperative)", function () {
 
     var sum = 0;
-    for(var i=1; i<1000; i+=1) {
-      if (i % 3 === 0 || i % 5 === 0) {
-        sum += i;
-      }
+    for (var i = 1; i < 1000; i += 1) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum += i;
+        }
     }
 
-    expect(sum).toBe(FILL_ME_IN);
-  });
+    expect(sum).toBe(233168);
+});
 
-  it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
+it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    var sum = _.range(1000) /* try chaining range() and reduce() */
+        .reduce( function(prev, cur) {
+            return (cur % 5 === 0 || cur % 3 === 0) ? prev + cur : prev; 
+        });
 
-    expect(233168).toBe(FILL_ME_IN);
-  });
+    expect(233168).toBe(sum);
+});
 
-  /*********************************************************************************/
-   it("should count the ingredient occurrence (imperative)", function () {
-    var ingredientCount = { "{ingredient name}": 0 };
+/*********************************************************************************/
+it("should count the ingredient occurrence (imperative)", function () {
+    var ingredientCount = {
+        "{ingredient name}": 0
+    };
 
-    for (i = 0; i < products.length; i+=1) {
-        for (j = 0; j < products[i].ingredients.length; j+=1) {
+    for (i = 0; i < products.length; i += 1) {
+        for (j = 0; j < products[i].ingredients.length; j += 1) {
             ingredientCount[products[i].ingredients[j]] = (ingredientCount[products[i].ingredients[j]] || 0) + 1;
         }
     }
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
-  });
+    expect(ingredientCount['mushrooms']).toBe(2);
+});
 
-  it("should count the ingredient occurrence (functional)", function () {
-    var ingredientCount = { "{ingredient name}": 0 };
+it("should count the ingredient occurrence (functional)", function () {
+    var ingredientCount = {
+        "{ingredient name}": 0
+    };
 
     /* chain() together map(), flatten() and reduce() */
+    ingredientCount = _(products).chain()
+        .map(function (x) { 
+            return x['ingredients'];
+        })
+        .flatten()
+        .reduce(function (prev, cur, index) {
+            if(index == 1) {
+                prev = {};
+            }
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
-  });
+            if (cur in prev) {
+                prev[cur]++;
+            } else {
+                prev[cur] = 1;
+            }
+            return prev;
+        }) 
+        .value();
 
-  /*********************************************************************************/
-  /* UNCOMMENT FOR EXTRA CREDIT */
-  /*
-  it("should find the largest prime factor of a composite number", function () {
+    expect(ingredientCount['mushrooms']).toBe(2);
+});
 
-  });
+/*********************************************************************************/
+/* UNCOMMENT FOR EXTRA CREDIT */
+/*
+it("should find the largest prime factor of a composite number", function () {
 
-  it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
+});
 
-  });
+it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
 
-  it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
+});
+
+it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
 
 
-  });
+});
 
-  it("should find the difference between the sum of the squares and the square of the sums", function () {
+it("should find the difference between the sum of the squares and the square of the sums", function () {
 
-  });
+});
 
-  it("should find the 10001st prime", function () {
+it("should find the 10001st prime", function () {
 
-  });
-  */
+});
+*/
 });
